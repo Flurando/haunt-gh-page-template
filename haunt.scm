be448e8e455b95b788f9ff17f262921b619d1b06
Flurando <185328747+Flurando@users.noreply.github.com>
2025-05-01 13:54:59 +0800

    basic set up

---
+++ haunt.scm
@@ -0,0 +1,22 @@
+(use-modules (haunt asset)
+             (haunt builder blog)
+             (haunt builder atom)
+             (haunt builder assets)
+             (haunt reader commonmark)
+	     (haunt reader skribe)
+             (haunt site))
+
+(site #:title "Title"
+      #:domain "example.domain"
+      #:posts-directory "post"
+      #:build-directory "site"
+      #:default-metadata
+      '((author . "Example")
+        (email  . "123@nota.email"))
+      #:readers (list commonmark-reader
+		      skribe-reader)
+      #:builders (list (blog #:prefix "/blog")
+                       (atom-feed)
+                       (atom-feeds-by-tag)
+                       (static-directory "image")))
+
