ad545f0d6b178744ce125c6659df299c0d76559c
Flurando <185328747+Flurando@users.noreply.github.com>
2025-05-02 06:34:25 +0800

    try absolute

--- haunt.scm
+++ haunt.scm
@@ -17,5 +17,5 @@
                        (atom-feed)
                        (atom-feeds-by-tag)
                        (static-directory "images"))
-      #:basepath "")
+      #:basepath "https://flurando.github.io/haunt-gh-page-template")
 
